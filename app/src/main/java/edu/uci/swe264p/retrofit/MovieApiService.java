c8671a601443d91255fa299d936aebb8d1dcf27f
Cheryl0402 <yi.cheryl.chen@gmail.com>
2022-02-24 21:55:03 -0800

    add a method to get top_rated movies

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieApiService.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieApiService.java
@@ -1,5 +1,7 @@
 package edu.uci.swe264p.retrofit;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -8,4 +10,9 @@ import retrofit2.http.Query;
 public interface MovieApiService {
     @GET("movie/{id}")
     Call<Movie> getMovie(@Path("id") int id, @Query("api_key") String apiKey);
+
+    // TODO :  add one more method for the above API
+    @GET("movie/top_rated")
+    Call<TopRatedResponse> getTopRatedResponse(@Query("api_key") String apiKey);
+    // returns a list of movies that are top rated.
 }
