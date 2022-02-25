17458c774b1bf2f2ac6d81f81164ccca9488ad00
Cheryl0402 <yi.cheryl.chen@gmail.com>
2022-02-24 21:57:11 -0800

    add get 20 movies

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
@@ -1,14 +1,68 @@
 package edu.uci.swe264p.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MovieListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    static final String TAG = MovieListActivity.class.getSimpleName();
+    private String BASE_URL = "https://api.themoviedb.org/3/";
+    static Retrofit retrofit = null;
+    final static String API_KEY = "dcdcde5d90ff7ac28e020269684102c5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        List<Movie> movieList = new ArrayList<>();
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<TopRatedResponse> call2 = movieApiService.getTopRatedResponse(API_KEY);
+        call2.enqueue(new Callback<TopRatedResponse>() {
+            @Override
+            public void onResponse(Call<TopRatedResponse> call2, Response<TopRatedResponse> response) {
+                for (int i = 0; i < 20; i++) {
+                    Float voteAverage = response.body().getResult().get(i).getVoteAverage();
+                    String posterPath = response.body().getResult().get(i).getPosterPath();
+                    String title = response.body().getResult().get(i).getTitle();
+                    String releaseDate = response.body().getResult().get(i).getReleaseDate().toString();
+                    String overView = response.body().getResult().get(i).getOverview();
+                    movieList.add(new Movie(voteAverage, posterPath, title, releaseDate, overView));
+                    System.out.println("======== movie: " + (i + 1) + " =============");
+                    System.out.println("Title: " + title + ". Vote: " + voteAverage);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TopRatedResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
+
+        recyclerView = findViewById(R.id.rvMovieList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MovieListAdapter(movieList));
+
     }
 }
