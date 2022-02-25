d2c4d9fbdca496f587331a9dced2889e916b339c
Cheryl0402 <yi.cheryl.chen@gmail.com>
2022-02-24 21:56:05 -0800

    implement TopRatedResponse class

--- app/src/main/java/edu/uci/swe264p/retrofit/TopRatedResponse.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/TopRatedResponse.java
@@ -1,4 +1,16 @@
 package edu.uci.swe264p.retrofit;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class TopRatedResponse {
+    @SerializedName("results")
+    private List<Movie> movieList;
+
+    public TopRatedResponse(List<Movie> list) {
+        movieList = list;
+    }
+
+    public List<Movie> getResult(){ return movieList;}
 }
